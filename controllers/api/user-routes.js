1cee3619a29783b68ccfc76c4d867920a9ec4aa3
Andrea Hergert <ahergert24@hotmail.com>
2022-11-15 14:26:52 -0800

    added required packages

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const { User } = require('../../models');
+
+// CREATE a new user
+router.post('/', async (req, res) => {
+  try {
+    const newUser = req.body;
+    // hash the password from 'req.body' and save to newUser
+    newUser.password = await bcrypt.hash(req.body.password, 10);
+    // create the newUser with the hashed password and save to DB
+    const userData = await User.create(newUser);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
