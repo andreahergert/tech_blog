4128825fe2e8618949d6e3745566c394264cddc4
Andrea Hergert <ahergert24@hotmail.com>
2022-11-18 11:46:40 -0800

    fixed index for api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- seeds/postData.js
+++ seeds/postData.js
@@ -4,7 +4,7 @@ const postdata =
 [
   {
     title: "USB drive",
-    content: "Plugging in a USB cable the right way feels like a no-brainer. Then why do we get it wrong on the first try so often? Here's the secret: Grab any USB cable sitting around your house. See that symbol on one side? It's not just branding or decoration. That symbol will point up if you're plugging in horizontally and if you're plugging a cable vertically, the USB symbol will face you. Now you know.",
+    content: "The symbol on one side will point up if you're plugging in horizontally and if you're plugging a cable vertically, the USB symbol will face you.",
     user_id: 1
   },
   {
@@ -14,7 +14,7 @@ const postdata =
   },
   {
     title: "Crop a screenshot",
-    content: "On a Mac, press Command + Shift + 5, and a rectangle will emerge, which you can manipulate as much as you want. On Windows 10, go to Start, enter the Search Bar, and type Snipping Tool. This will give you a similar box that you can drag to any shape over your screen.",
+    content: "On a Mac, press Command + Shift + 5, and a rectangle will emerge, which you can manipulate as much as you want. On Windows 10, go to Start, enter the Search Bar, and type Snipping Tool.",
     user_id: 3
   },
 ];
