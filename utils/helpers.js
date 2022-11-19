8403baf749935bbb8fda48b35ca9adde66a5dd72
Andrea Hergert <ahergert24@hotmail.com>
2022-11-19 14:47:19 -0800

    formatting changes

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,8 +3,8 @@ module.exports = {
     return date.toLocaleTimeString();
   },
   format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${        
-      new Date(date).getFullYear()
-    }`;
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
   },
 };
