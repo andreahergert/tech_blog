f02ee82c79186ccf521f9c7a97f04278bb01f035
Andrea Hergert <ahergert24@hotmail.com>
2022-11-18 19:07:36 -0800

    updated utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,10 @@
 module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${        
+      new Date(date).getFullYear()
+    }`;
   },
 };
