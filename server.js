d8bc49f32587afa93da4e0cba9773aef5eaf27fb
Andrea Hergert <ahergert24@hotmail.com>
2022-11-19 18:13:03 -0800

    updated connections for heroku

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'supersecret',
-  cookie: {},
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -26,6 +31,7 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -36,7 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(`Now listening on http://localhost:${PORT} `)
-  );
+  app.listen(PORT, () => console.log('Now listening on http://localhost:${PORT}'));
 });
