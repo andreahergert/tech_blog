d3850356da7918e27763b9254cb33ac61a997371
Andrea Hergert <ahergert24@hotmail.com>
2022-11-20 12:53:51 -0800

    updated cookies

--- server.js
+++ server.js
@@ -15,12 +15,8 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  secret: 'supersecret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
