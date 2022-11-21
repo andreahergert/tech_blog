5232544b0d8b9bb798ab94611fb13fa849e7158b
Andrea Hergert <ahergert24@hotmail.com>
2022-11-21 12:09:03 -0800

    heroku error

--- config/connection.js
+++ config/connection.js
@@ -1,46 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
-  }
-);
+  );
+}
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
-  
-// local connection
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
+module.exports = sequelize;
