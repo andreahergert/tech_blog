8d7a3e38ebd9a5c59799e4e1080ab3f7c622c99b
Andrea Hergert <ahergert24@hotmail.com>
2022-11-21 10:09:49 -0800

    minor console changes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
 
 // let port = process.env.PORT;
