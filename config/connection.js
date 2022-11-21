65f9d39f025c10783d7d326c4a2315e26a4ec9b9
Andrea Hergert <ahergert24@hotmail.com>
2022-11-21 09:23:05 -0800

    changed port

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.PORT) {
+  sequelize = new Sequelize(process.env.PORT);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -13,26 +13,9 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 8000
-      // port: 3306
+      port: 3306
     }
   );
 }
 
 module.exports = sequelize;
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
-
-// module.exports = sequelize;
--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-// const PORT = process.env.PORT || 3001;
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -45,3 +41,8 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
