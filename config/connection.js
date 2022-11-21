e2e3ee6b217a72b72f7929ea7be5859bdf4128e3
Andrea Hergert <ahergert24@hotmail.com>
2022-11-21 12:03:09 -0800

    heroku error

--- config/connection.js
+++ config/connection.js
@@ -10,15 +10,6 @@ sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 );
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
 
   
 // local connection
